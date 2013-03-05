2b43dad132bb82e3cb8b8a96cf18eeb8b6f468fa
epithatree <epithatree@gmail.com>
2013-03-05 17:55:57 +0000

    questions 1 and 2

--- scala/HammingCode.scala
+++ scala/HammingCode.scala
@@ -1,7 +1,6 @@
-
-
 import  ox.CSO._
 import  ox.cso.Components
+import scala.collection.immutable
 
 object HammingCode{
 
@@ -15,32 +14,44 @@ object HammingCode{
     repeat{
       val v = in?; out!(2*v);
     }
+  in.closein; out.closeout;
   }
 
   def x3(in: ?[Int], out: ![Int]) : PROC = proc{
       repeat{
       val v = in?; out!(3*v);
     }
+  in.closein; out.closeout;
   }
 
   def x5(in: ?[Int], out: ![Int]) : PROC = proc{
     repeat{
       val v = in?; out!(5*v);
     }
+  in.closein; out.closeout;
   }
 
   def Merge(in1: ?[Int], in2: ?[Int], out: ![Int]) : PROC = proc{
+  var table = new immutable.TreeSet[Int]() 
   repeat {
-        (proc{out!(in1?)} || proc{out!(in2?)}) ()
+    alt(in1 --> {val v = in1?; 
+                if (!table.contains(v))
+ {out!(v); table=(table+v)}}
+      | in2 --> {val v = in2?; 
+                if (!table.contains(v))
+ {out!(v); table=(table+v)}}
+       )
         }
   in1.closein; in2.closein; out.closeout;
   }
 
   def Prefix1(in: ?[Int], out: ![Int]) : PROC = proc{
-  out!1;
-  repeat {
-        out!(in?)
+  var n = 0; 
+  out!1; n+=1;
+  for (n <- 1 to 1000){
+        out!(in?); 
         }
+  Console.println("done")
   in.closein; out.closeout;
   }
   
@@ -48,7 +59,7 @@ object HammingCode{
   def TheConsole : PROC = proc{ repeat{ Console.println(output?) } }
 
   val x2Input, x3Input, x5Input, x2Output, x3Output, x5Output, mergeToMerge, mergeToPrefix, tee1ToTee2, tee2ToTee3, output = OneOne[Int];
-  val prefixToTee = Buf[Int](4);
+  val prefixToTee = Buf[Int](1000);
 
   def System =
     Prefix1(mergeToPrefix, prefixToTee) || Tee(prefixToTee, tee1ToTee2, output) || Tee(tee1ToTee2, tee2ToTee3, x2Input)  || Tee(tee2ToTee3, x3Input, x5Input) || x2(x2Input, x2Output) || x3(x3Input, x3Output) || x5(x5Input, x5Output) || Merge(x2Output, x3Output, mergeToMerge) || Merge(mergeToMerge, x5Output, mergeToPrefix)||TheConsole
---
+++ scala/TheGameOfLife.scala
@@ -0,0 +1,6 @@
+val N = 5;
+
+val a = new Array[Array[Boolean]](N,N)
+val display= new Display(N,a)
+
+display.draw
